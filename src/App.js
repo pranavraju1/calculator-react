9945422ff6a2326319d8554f77ff28c2d2a369be
Pranav Raju <pranavraju2020@gmail.com>
2023-11-27 21:54:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+const App = () => {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [message, setMessage] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [flag,setFlag] = useState('');
+  // console.log(num1);
+  // console.log(num2);
 
-function App() {
+  const handleOperation = (opration) => {
+    if (!num1) {
+      setMessage("num1 is empty");
+    } else if (!num2) {
+      setFlag(false)
+      setMessage("num2 is empty");
+    } else {
+      setFlag(true)
+      setMessage("success");
+      switch (opration) {
+        case "+":
+          setAnswer(num1 + num2);
+          break;
+        case "-":
+          setAnswer(num1 - num2);
+          break;
+        case "/":
+          setAnswer(num1 / num2);
+          break;
+        case "x":
+          setAnswer(num1 * num2);
+          break;
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="card">
+        <div style={{ fontSize: "20px", fontWeight: "bold" }}>
+          React Calculator
+        </div>
+        <div>
+          <form>
+            <input
+              type="text"
+              placeholder="NUM1"
+              onChange={(e) => setNum1(parseInt(e.target.value))}
+            ></input>
+            <input
+              type="text"
+              placeholder="NUM2"
+              onChange={(e) => setNum2(parseInt(e.target.value))}
+            ></input>
+          </form>
+        </div>
+        <div className="opperations">
+          <button onClick={() => handleOperation("+")}>+</button>
+          <button onClick={() => handleOperation("-")}>-</button>
+          <button onClick={() => handleOperation("/")}>/</button>
+          <button onClick={() => handleOperation("x")}>x</button>
+        </div>
+        {
+          flag && <div style={{ color: "green" }}>{message}</div>
+        }
+        {
+          (!flag) && <div style={{ color: "red" }}>{message}</div>
+        }
+        <div>{answer}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
